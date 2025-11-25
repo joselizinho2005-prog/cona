53d77203d96297360fecf793342fc7fcc91ce81b
joselizinho2005-prog <joselizinho2005@gmail.com>
2025-11-25 15:22:35 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
